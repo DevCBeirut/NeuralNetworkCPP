d37ff9f8194e947656a9f353650dbe0d2dc102b3
tony6862 <31767826+tony6862@users.noreply.github.com>
2018-09-15 01:39:10 +0300

    Delete INeuralNetwork.cpp

--- NeuralNetworkCPP/NeuralNetwork/INeuralNetwork.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  INeuralNetwork.cpp
-//  NeuralNetworkCPP
-//
-//  Created by MAC on 9/15/18.
-//  Copyright © 2018 Tony Abou Zaidan. All rights reserved.
-//
-
-#include "INeuralNetwork.hpp"
